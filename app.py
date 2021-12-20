4614407a704720188ced48624f3b16056e0a460a
킹래빗 <skeptic719@gmail.com>
2021-12-20 15:54:19 +0900

    jinja2 template rev

--- app.py
+++ app.py
@@ -18,9 +18,16 @@ def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
+        entries.append((entry_content, formatted_date))
+
+        entries_with_date = [
+            (entry[0], entry[1], datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d"))
+            for entry in entries
+        ]
+
+        
     
-    return render_template("index.html")
+    return render_template("index.html", entries=entries_with_date)
 
 
 @app.route("/first_page")
