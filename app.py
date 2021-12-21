d0a3eb2619983e353e063be862cff4217ceaf6a7
킹래빗 <skeptic719@gmail.com>
2021-12-21 16:54:12 +0900

    db retrieving

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import datetime
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://sykim:1234@whitespace.2rxid.mongodb.net/test?authSource=admin&replicaSet=atlas-1x7et7-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
 
+app.db = client.whitespace
 
 entries = []
 
@@ -16,14 +19,15 @@ class GalileanMoons:
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formatted_date))
+        app.db.posts.insert({"content":entry_content, "date":formatted_date})
 
     entries_with_date = [
-            ( entry[0], entry[1], datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d") )
-            for entry in entries
+            ( entry['content'], entry['date'], datetime.datetime.strptime(entry['date'], "%Y-%m-%d").strftime("%b %d") )
+            for entry in app.db.posts.find({},)
             ]
 
     return render_template("index.html", entries=entries_with_date)
